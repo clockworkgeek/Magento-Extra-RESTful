3e99ce5dd9755b7bc9b0ba8a0b393b6a8f997fa8
Daniel Deady <daniel@clockworkgeek.com>
2018-02-28 22:28:22 +0000

    Clean up unused function

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -2,11 +2,4 @@
 
 class Clockworkgeek_Extrarestful_Helper_Data extends Mage_Core_Helper_Data
 {
-
-    public function isCollectionOverflowed(Varien_Data_Collection $collection, Mage_Api2_Model_Request $request)
-    {
-        $pageSize = (int) $request->getPageSize() ?: Mage_Api2_Model_Resource::PAGE_SIZE_DEFAULT;
-        $pageNum = ((int) $request->getPageNumber() ?: 1) - 1;
-        return $pageSize * $pageNum >= $collection->getSize();
-    }
 }
