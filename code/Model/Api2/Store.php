65e4452c83135c2d4c69db382949d1005b3119ae
Daniel Deady <daniel@clockworkgeek.com>
2018-03-02 23:50:37 +0000

    Store resource is descendant of common base class
    Adds paging and filtering while simplifying code.

--- code/Model/Api2/Store.php
+++ code/Model/Api2/Store.php
@@ -1,17 +1,5 @@
 <?php
 
-class Clockworkgeek_Extrarestful_Model_Api2_Store extends Mage_Api2_Model_Resource
+class Clockworkgeek_Extrarestful_Model_Api2_Store extends Clockworkgeek_Extrarestful_Model_Api2_Abstract
 {
-
-    protected function _retrieveCollection()
-    {
-        $stores = array();
-        /* @var $store Mage_Core_Model_Store */
-        foreach (Mage::app()->getStores() as $id => $store) {
-            if ($store->getIsActive()) {
-                $stores[$id] = $store->getData();
-            }
-        }
-        return $stores;
-    }
 }
--- code/Model/Api2/Store/Rest/Customer/V1.php
+++ code/Model/Api2/Store/Rest/Customer/V1.php
@@ -3,4 +3,12 @@
 class Clockworkgeek_Extrarestful_Model_Api2_Store_Rest_Customer_V1
 extends Clockworkgeek_Extrarestful_Model_Api2_Store
 {
+
+    protected function _getCollection()
+    {
+        /** @var $stores Mage_Core_Model_Resource_Store_Collection */
+        $stores = parent::_getCollection();
+        $stores->addFieldToFilter('is_active', true);
+        return $stores;
+    }
 }
