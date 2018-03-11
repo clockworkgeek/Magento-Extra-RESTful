245c62d533f25c293f7df771a590f36ed619040e
Daniel Deady <daniel@clockworkgeek.com>
2018-03-11 11:49:09 +0000

    Omit collection keys from JSON
    Varien collections naturally use their items' IDs as array keys.
    When output as JSON these become object keys but objects are unordered.
    A client iterating over JSON might get the order wrong.
    XML output already drops the keys and preserves the order,
    it's only fair to keep the results as similar as possible.

--- code/Model/Api2/Abstract.php
+++ code/Model/Api2/Abstract.php
@@ -114,7 +114,9 @@ class Clockworkgeek_Extrarestful_Model_Api2_Abstract extends Mage_Api2_Model_Res
         else {
             $this->_loadCollection($collection);
             $data = $collection->toArray();
-            return (array) (@$data['items'] ?: $data);
+            // an array with non-consecutive keys is an unordered object when rendered as JSON
+            // array_values effectively gives new keys and preserves order to the client
+            return array_values((array) (@$data['items'] ?: $data));
         }
     }
 
