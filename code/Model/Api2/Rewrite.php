c20ea4dc7364c50f8d7988f9a2170b960756fad9
Daniel Deady <daniel@clockworkgeek.com>
2018-04-17 17:09:19 +0100

    URL Rewrites cannot be localised
    There is no route with store.
    Rewrites still have a `store_id` field.
    Collections can still be filtered by `store_id`.

--- code/Model/Api2/Rewrite.php
+++ code/Model/Api2/Rewrite.php
@@ -8,15 +8,4 @@
  */
 class Clockworkgeek_Extrarestful_Model_Api2_Rewrite extends Clockworkgeek_Extrarestful_Model_Api2_Abstract
 {
-
-    protected function _getCollection()
-    {
-        /** @var $rewrites Mage_Core_Model_Resource_Url_Rewrite_Collection */
-        $rewrites = parent::_getCollection();
-        if (! is_null($storeId = $this->getRequest()->getParam('store'))) {
-            $rewrites->addStoreFilter($storeId);
-        }
-
-        return $rewrites;
-    }
 }
