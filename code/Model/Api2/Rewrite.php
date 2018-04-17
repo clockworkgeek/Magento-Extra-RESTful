2c245f3ebff97fb9f1bf4e73c21070d7637dea57
Daniel Deady <daniel@clockworkgeek.com>
2018-04-17 13:41:24 +0100

    System URL rewrites are viewable with custom URL rewrites
    There's no difference between the types anyway.
    No need to make a distinction.
    Drop "Custom" from the resource title since it's all rewrite types now.

--- code/Model/Api2/Rewrite.php
+++ code/Model/Api2/Rewrite.php
@@ -13,7 +13,6 @@ class Clockworkgeek_Extrarestful_Model_Api2_Rewrite extends Clockworkgeek_Extrar
     {
         /** @var $rewrites Mage_Core_Model_Resource_Url_Rewrite_Collection */
         $rewrites = parent::_getCollection();
-        $rewrites->addFieldToFilter('is_system', 0);
         if (! is_null($storeId = $this->getRequest()->getParam('store'))) {
             $rewrites->addStoreFilter($storeId);
         }
