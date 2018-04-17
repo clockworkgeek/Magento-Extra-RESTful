9c18d87a061e77bf22b483ab3e8f72323eb04b93
Daniel Deady <daniel@clockworkgeek.com>
2018-04-17 21:42:09 +0100

    Admin can write URL rewrites
    Usual CRUD actions are supported.
    Several intelligent behaviours are implemented, for example,
    a missing request path can be filled in from normal Magento patterns.
    ID paths are still not exposed but deduced from product and category.

--- code/Model/Api2/Rewrite.php
+++ code/Model/Api2/Rewrite.php
@@ -5,7 +5,88 @@
  *
  * @author Daniel Deady <daniel@clockworkgeek.com>
  * @license MIT
+ * @method Mage_Core_Model_Url_Rewrite _loadModel()
  */
 class Clockworkgeek_Extrarestful_Model_Api2_Rewrite extends Clockworkgeek_Extrarestful_Model_Api2_Abstract
 {
+
+    protected function _saveModel($data)
+    {
+        $rewrite = $this->_loadModel();
+        $rewrite->addData($data);
+        /** @var $catalogUrl Mage_Catalog_Model_Url */
+        $catalogUrl = Mage::getModel('catalog/url');
+
+        if (!$rewrite->getStoreId()) {
+            $this->_error('Store ID is required', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+        }
+
+        if (!in_array($rewrite->getOptions(), array(null, '', 'R', 'RP'))) {
+            $this->_error('Options must be one of "", "R", "RP"', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+        }
+
+        if ($rewrite->getProductId()) {
+            // only load minimum attributes necessary
+            $product = Mage::getModel('catalog/product')->load($rewrite->getProductId(), array('entity_id', 'url_key'));
+            // should not be possible to have a bad entity ID because of database's foreign keys but check anyway
+            if ($product->isObjectNew()) {
+                $this->_error('Product ID does not exist', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+            }
+        }
+        else {
+            $product = null;
+        }
+
+        if ($rewrite->getCategoryId()) {
+            $category = Mage::getModel('catalog/category')->load($rewrite->getCategoryId(), array('entity_id', 'parent_id', 'level', 'url_key'));
+            if ($category->isObjectNew()) {
+                $this->_error('Category ID does not exist', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+            }
+        }
+        else {
+            $category = null;
+        }
+
+        // empty paths are allowed, nulls and complex types are not
+        if (!is_scalar($rewrite->getRequestPath())) {
+            // try to fill in missing path
+            if ($category) {
+                // category path lookup requires a frontend store ID or there will be fatal error
+                $category->setStoreId($rewrite->getStoreId() ?: Mage::app()->getDefaultStoreView()->getId());
+                $rewrite->setRequestPath($catalogUrl->generatePath('request', $product, $category));
+            }
+            else {
+                $this->_error('Request path is required', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+            }
+        }
+
+        if (!is_scalar($rewrite->getTargetPath())) {
+            if ($product || $category) {
+                $rewrite->setTargetPath($catalogUrl->generatePath('target', $product, $category));
+            }
+            else {
+                $this->_error('Target path is required', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+            }
+        }
+
+        // mimic normal catalog ID paths
+        if ($product || $category) {
+            $rewrite->setIdPath($catalogUrl->generatePath('id', $product, $category));
+            // compare new path against existing paths
+            if (!$rewrite->getId() && $rewrite->getResource()->getRequestPathByIdPath($rewrite->getIdPath(), $rewrite->getStoreId())) {
+                $this->_error('A rewrite already exists for this product/category and store', 409);
+            }
+        }
+        else {
+            // this path will not be used so it can be random
+            // overwrite any previous value just in case it was related to catalog
+            $rewrite->setIdPath($catalogUrl->generateUniqueIdPath());
+        }
+
+        if (!$this->getResponse()->isException()) {
+            // only internally generated requests may be system type
+            $rewrite->setIsSystem(false)->save();
+        }
+        return $rewrite;
+    }
 }
--- docs/URLRewrites.md
+++ docs/URLRewrites.md
@@ -17,9 +17,70 @@ If a match is found then:
 ### Attributes
 
 - `category_id`: If not null, this rewrite will be used to build canonical URLs for categories.
-- `options`: Type of redirect.  One of "" (no), "R" (temporary), "RP" (permanent).
+- `options`: Type of redirect.  One of "" (internal), "R" (temporary), "RP" (permanent).
 - `product_id`: If not null, this rewrite will be used to build canonical URLs for products.
 - `request_path`: Incoming requests are matched against this string.
 - `store_id`: The store where this rewrite applies.
 - `target_path`: Matching requests are redirected to this full or partial URI.
 - `url_rewrite_id`: ID of this rewrite.
+
+## Create / Update
+
+- `POST /api/rest/rewrites`
+- `PUT /api/rest/rewrites/:id`
+
+### Attributes
+
+- `category_id`: If not null, this rewrite will be used to build canonical URLs for categories.
+- `options`: Type of redirect.  One of "" (internal), "R" (temporary), "RP" (permanent).
+- `product_id`: If not null, this rewrite will be used to build canonical URLs for products.
+- `request_path`: Incoming requests are matched against this string.
+- `store_id`: **Required**. The store where this rewrite applies.
+- `target_path`: Matching requests are redirected to this full or partial URI.
+
+### Examples
+
+In the first example an HTTP redirect is created to an external site.
+It is not necessary to specify the type of redirect because it defaults to a temporary redirect when the target path is a fully qualified URL.
+There is an `Authorization` header because this is an admin action.
+
+```http
+POST /api/rest/rewrites HTTP/1.1
+Authorization: XXXXXXXXX
+Content-Length: 75
+Content-Type: application/json
+Host: example.com
+
+{
+    "request_path": "readme",
+    "store_id": 1,
+    "target_path": "https://readme.com/"
+}
+
+HTTP/1.1 202 Accepted
+Location: /api/rest/rewrites/555
+```
+
+Now consider the task of converting a menu item into a link to an external site.
+The popular method would be to create a category as appropriate,
+then delete it's rewrite entry (because it is "system" and cannot be altered),
+then create a replacement record with a duplicate "ID Path".
+Here you may simply update the rewrite's target and it will automatically be changed from "system" to "custom".
+
+```http
+PUT /api/rest/rewrites/777 HTTP/1.1
+Authorization: XXXXXXXXX
+Content-Length: 38
+Content-Type: application/json
+Host: example.com
+
+{
+    "target_path": "https://readme.com/"
+}
+
+HTTP/1.1 200 OK
+```
+
+## Delete
+
+- `DELETE /api/rest/rewrites/:id`
